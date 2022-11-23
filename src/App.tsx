729d13ebe1d543af876cd245db5d4145d8aa1a97
river <rivercx@proton.me>
2022-11-24 00:16:46 +0800

    feat: json-server middleware

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ProjectListScreen from "screens/project-list";
+import { LoginScreen } from "screens/login";
+import { loadDevTools } from "jira-dev-tool";
 
 function App() {
   return (
     <>
+      <LoginScreen />
       <ProjectListScreen />
     </>
   );
