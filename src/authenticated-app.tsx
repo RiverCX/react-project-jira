01bf6f7ba7f3e848f5c3e5addf5e836ef31a51a2
river <rivercx@proton.me>
2022-11-25 01:32:45 +0800

    style: add Row component

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,13 +1,41 @@
 import { useAuth } from "context/auth-context";
 import React from "react";
 import ProjectListScreen from "screens/project-list";
+import styled from "@emotion/styled";
+import { Row } from "components/lib";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h3>Logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+
+const HeaderItem = styled.h3`
+  margin-right: 3rem;
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
+
+const Header = styled(Row)``;
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled.div``;
+const Main = styled.main``;
