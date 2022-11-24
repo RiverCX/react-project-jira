7604e8c73823d1e0647005482e25b5bb3a3dc8f0
river <rivercx@proton.me>
2022-11-24 21:36:03 +0800

    feat: use antd

--- src/screens/project-list/SearchPanel.tsx
+++ src/screens/project-list/SearchPanel.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Input, Select } from "antd";
 export interface User {
   id: string;
   name: string;
@@ -21,22 +22,22 @@ const SearchPanel = ({ param, setParam, users }: SearchPanelProps) => {
   return (
     <form action="">
       <div>
-        <input
+        <Input
           type="text"
           value={param.name}
           onChange={(e) => setParam({ ...param, name: e.target.value })}
         />
-        <select
+        <Select
           value={param.personId}
-          onChange={(e) => setParam({ ...param, personId: e.target.value })}
+          onChange={(value) => setParam({ ...param, personId: value })}
         >
-          <option value="">负责人</option>
+          <Select.Option value="">负责人</Select.Option>
           {users.map((user) => (
-            <option value={user.id} key={user.id}>
+            <Select.Option value={user.id} key={user.id}>
               {user.name}
-            </option>
+            </Select.Option>
           ))}
-        </select>
+        </Select>
       </div>
     </form>
   );
--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,31 +1,39 @@
 import { useAuth } from "context/auth-context";
 import React from "react";
-import { FormEvent } from "react";
+import { Form, Input, Button } from "antd";
 
 // 注册组件
 export const RegisterScreen = () => {
   const { register } = useAuth();
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const username = (event.currentTarget.elements[0] as HTMLInputElement)
-      .value;
-    const password = (event.currentTarget.elements[1] as HTMLInputElement)
-      .value;
-    register({ username, password });
+  const handleSubmit = (values: { username: string; password: string }) => {
+    register(values);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="username">用户名</label>
-        <input type="text" name="username" id="username" />
-      </div>
-      <div>
-        <label htmlFor="password">密码</label>
-        <input type="password" name="password" id="password" />
-      </div>
-      <button type="submit">注册</button>
-    </form>
+    <Form onFinish={handleSubmit}>
+      <Form.Item
+        name={"username"}
+        rules={[{ required: true, message: "请输入用户名" }]}
+      >
+        <Input placeholder="用户名" type="text" name="username" id="username" />
+      </Form.Item>
+      <Form.Item
+        name={"password"}
+        rules={[{ required: true, message: "请输入密码" }]}
+      >
+        <Input
+          placeholder={"密码"}
+          type="password"
+          name="password"
+          id="password"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button type={"primary"} htmlType={"submit"}>
+          注册
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
