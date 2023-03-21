c1529d7d839c4b7ac17b4e6132a35e13c474bc30
river <rivercx@proton.me>
2023-03-22 05:00:16 +0800

    chore: 规范文件名

---
+++ src/routes/index.tsx
@@ -0,0 +1,39 @@
+import { useRoutes } from "react-router-dom";
+import { EpicScreen } from "screens/epic";
+import { KanbanScreen } from "screens/kanban";
+import { ProjectScreen } from "screens/project";
+import { ProjectListScreen } from "screens/project-list";
+
+export const useRouter = () => {
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: <ProjectListScreen />,
+    },
+    {
+      path: "/projects",
+      element: <ProjectListScreen />,
+    },
+    {
+      path: "/projects/:projectId",
+      element: <ProjectScreen />,
+      children: [
+        { index: true, element: <KanbanScreen /> },
+        {
+          path: "kanban",
+          element: <KanbanScreen />,
+        },
+        {
+          path: "epic",
+          element: <EpicScreen />,
+        },
+      ],
+    },
+    {
+      path: "*",
+      element: <ProjectListScreen />,
+    },
+  ]);
+
+  return routes;
+};
