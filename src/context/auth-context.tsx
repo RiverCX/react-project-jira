314cb52c339096ae0417569a285e22c3a767eb34
river <rivercx@proton.me>
2023-03-25 22:50:18 +0800

    feat: 登出时清空query缓存

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -4,6 +4,7 @@ import * as auth from "auth-provider";
 import { http } from "utils/http";
 import { useAsync } from "utils/use-async";
 import { FullPageErrorFallback, FullPageLoading } from "components/lib";
+import { useQueryClient } from "react-query";
 
 interface AuthForm {
   username: string;
@@ -48,9 +49,14 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   } = useAsync<User | null>();
 
   // 在原来的fetch请求处理基础上，加上user状态的处理
+  const queryClient = useQueryClient();
   const login = (form: AuthForm) => auth.login(form).then(setUser);
   const register = (form: AuthForm) => auth.register(form).then(setUser);
-  const logout = () => auth.logout().then(() => setUser(null));
+  const logout = () =>
+    auth
+      .logout()
+      .then(() => setUser(null))
+      .then(() => queryClient.clear());
 
   // 初次渲染时 user初始化
   useEffect(() => {
