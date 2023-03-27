ac17aa1aa3295a61dbe1554e265e71a80691856a
river <rivercx@proton.me>
2023-03-27 12:00:33 +0800

    feat: 任务搜索和样式优化

--- src/screens/project/index.tsx
+++ src/screens/project/index.tsx
@@ -1,11 +1,39 @@
-import { Link, Outlet } from "react-router-dom";
+import styled from "@emotion/styled";
+import { Menu } from "antd";
+import { NavLink, Outlet } from "react-router-dom";
 
 export const ProjectScreen = () => {
   return (
-    <div>
-      <Link to={"kanban"}>看板</Link>
-      <Link to={"epic"}>任务组</Link>
-      <Outlet />
-    </div>
+    <Container>
+      <Aside>
+        <Menu mode="inline">
+          <Menu.Item>
+            <NavLink to={"kanban"}>看板</NavLink>
+          </Menu.Item>
+          <Menu.Item>
+            <NavLink to={"epic"}>任务组</NavLink>
+          </Menu.Item>
+        </Menu>
+      </Aside>
+      <Main>
+        <Outlet />
+      </Main>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 16rem 1fr;
+`;
+
+const Aside = styled.aside`
+  background-color: rgb(244, 245, 247);
+  display: flex;
+`;
+
+const Main = styled.div`
+  display: flex;
+  padding: 3.2rem;
+  box-shadow: -5px 0 5px -5px rgba(0, 0, 0, 0.1);
+`;
